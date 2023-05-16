dc4877a5a5ac4a20c79132477185dc7fdbff11b4
Tim Becker <tim.becker@kellogg.northwestern.edu>
2023-05-16 15:56:06 +0000

    Testing views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,22 @@
 class PostsController < ApplicationController
-  def index
+  def new
+    @place = Place.find(params[:place_id])
+    @post = Post.new
   end
+
+  def create
+    @place = Place.find(params[:place_id])
+    @post = @place.posts.new(post_params)
+    if @post.save
+      redirect_to place_path(@place)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
-  
+    get("/", { :controller => "tacos", :action => "index" })
     resources "places"
     resources "posts"
 
